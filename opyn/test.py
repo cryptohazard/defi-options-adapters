981260455a975ed4e83d2c4e0dc7684ea5c7a9b1
cryptohazard <jyezie@yahoo.fr>
2024-05-24 23:43:15 +0200

    DEV: add Collateral ratio func

--- opyn/test.py
+++ opyn/test.py
@@ -9,8 +9,8 @@ def __():
     from web3 import Web3
     import json
     from uniswap import Uniswap
-
-    return Uniswap, Web3, json
+    import decimal
+    return Uniswap, Web3, decimal, json
 
 
 @app.cell
@@ -26,30 +26,22 @@ def __(Uniswap, rpc):
     # constants
     version = 3                       # specify which version of Uniswap to use
     uniswap = Uniswap(address='',private_key='',provider=rpc, version = version)
-    eth = "0x0000000000000000000000000000000000000000"
-    weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    ether = "0x0000000000000000000000000000000000000000"
     osqth = "0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B"
     usdc= "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 
-    usdcaddress      ='0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-    wethaddress      ='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    squeethaddress   = '0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B'
     controlleraddress= '0x64187ae08781B09368e6253F9E94951243A493D5'
     quoteraddress    = '0xC8d3a4e6BB4952E3658CCA5081c358e6935Efa43'
     shortpowerperpaddress     = '0xa653e22A963ff0026292Cc8B67941c0ba7863a38'
     return (
         controlleraddress,
-        eth,
+        ether,
         osqth,
         quoteraddress,
         shortpowerperpaddress,
-        squeethaddress,
         uniswap,
         usdc,
-        usdcaddress,
         version,
-        weth,
-        wethaddress,
     )
 
 
@@ -76,9 +68,10 @@ def __(abi, controlleraddress, w3):
 
 
 @app.cell
-def __(controller):
-    controller.functions.vaults(389).call()
-    return
+def __(controller, decimal):
+    norm = decimal.Decimal(controller.functions.normalizationFactor().call()/10**18)
+    norm
+    return norm,
 
 
 @app.cell
@@ -118,29 +111,7 @@ def __(state, w3):
             round(w3.from_wei(s[3],'ether'),4)
             ])
     ps
-        
     return ps, s
-
-
-@app.cell
-def __():
-    crabv1=70  #vault of crab v1.
-    crab=286 #vault of crab v2
-    return crab, crabv1
-
-
-@app.cell
-def __(crab, state, w3):
-    bal = w3.from_wei(state[crab][2],'ether')
-    debt= w3.from_wei(state[crab][3],'ether')
-    print(f'The Crab strategy has {bal} ETH of collateral and {debt} SQUEETH of debt!')
-    return bal, debt
-
-
-@app.cell
-def __(ps):
-    type(ps)
-    return
 
 
 @app.cell
@@ -158,13 +129,45 @@ def __(ps):
 
 
 @app.cell
-def __(ps):
+def __(decimal, ether, osqth, uniswap):
+    price_squeeth_weth = decimal.Decimal(uniswap.get_price_input(osqth, ether, 1*10**18, fee=3000)/10**18)
+    price_squeeth_weth
+    return price_squeeth_weth,
+
+
+@app.cell
+def __(decimal, ether, uniswap, usdc):
+    price_usdc = decimal.Decimal(uniswap.get_price_input(ether, usdc, 1*10**18, fee=3000)/10**6)
+    price_usdc
+    return price_usdc,
+
+
+@app.cell
+def __(price_squeeth_weth, price_usdc):
+    price_squeeth= price_squeeth_weth * price_usdc
+    price_squeeth
+    return price_squeeth,
+
+
+@app.cell
+def __(norm, price_squeeth):
+    def ratio(v):
+        r = v[3] * price_squeeth * norm/v[2]
+        return r
+
+    return ratio,
+
+
+@app.cell
+def __(ps, ratio):
     def hasdebt(l: list):
         result=[]
+        
         for s in l:
             if s[3] != 0:
                 result.append(s)
-                print(f'vault {ps.index(s)}: {s}')
+                cr = ratio(s)
+                print(f'vault {ps.index(s)}: Coll. ratio {cr:.2f} {s}')
         return result
 
     vaults=hasdebt(ps)
@@ -172,16 +175,30 @@ def __(ps):
 
 
 @app.cell
-def __(eth, osqth, uniswap):
-    uniswap.get_price_input(eth, osqth, 1*10**18, fee=3000)/10**18
-    return
+def __():
+    crabv1=70  #vault of crab v1.
+    crab=286 #vault of crab v2
+    threshold = 1.5 #150%
+    return crab, crabv1, threshold
 
 
 @app.cell
-def __(eth, uniswap, usdc):
-    uniswap.get_price_input(eth, usdc, 1*10**18, fee=3000)/10**6
+def __(crab, ratio, state, w3):
+    bal = w3.from_wei(state[crab][2],'ether')
+    debt= w3.from_wei(state[crab][3],'ether')
+    print(f'The Crab strategy has {bal:.2f} ETH of collateral \nand {debt:.2f} SQUEETH of debt!')
+    print(f'This brings its collateral ratio to {ratio(state[crab]):.2f}.')
+    return bal, debt
 
-    return
+
+@app.cell
+def __(norm, price_squeeth, price_usdc):
+    p2 = price_usdc*price_usdc
+    diff = (10000*price_squeeth/norm-p2)/p2
+    100*diff
+    print(f'The current diff between Squeeth and ETH^2 is {100*diff:.2f}%.')
+
+    return diff, p2
 
 
 @app.cell
